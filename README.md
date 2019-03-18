2bec6ad1963120b414782157f95a55371e75a170
schmitzaddie <schmitzaddie@gmail.com>
2019-03-18 18:24:11 -0500

    Updating files

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # bamazon
+
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products(
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(100) NOT NULL,
+price INT(10) NOT NULL,
+stock_quantity INT(10) NOT NULL,
+PRIMARY KEY (item_id)
+);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUE ("Banana", "Produce", 2, 30);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUE ("Frozen Pizza", "Frozen", 18, 10);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUE ("Red Bull", "Beverages", 3, 40);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUE ("Bagel", "Bakery", 2, 12);
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUE ("Chocolate", "Candy", 1, 40);
---
+++ bamazonCustomer.js
@@ -0,0 +1,95 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Luckydog13!!",
+    database: "bamazon_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw (err);
+    console.log("connected as id " + connection.threadId + "\n");
+    productList();
+});
+
+function productList() {
+    connection.query("SELECT * FROM products", function (err, data) {
+        if (err) throw (err);
+        console.log("Check out all the products we carry! \n");
+        
+        for (var i = 0; i < data.length; i++) {
+            console.log(data[i].item_id + " | " + data[i].product_name + " | " + data[i].department_name + " | $" + data[i].price + " | In-Stock: " + data[i].stock_quantity);
+        }
+        console.log("");
+        customerShop();
+    });
+}
+
+function customerShop() {
+    connection.query("SELECT * FROM products", function (err, data) {
+        if (err) throw (err);
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "product",
+                message: "What would you like to purchase? Please provide the ID #.",
+            },
+            {
+                type: "input",
+                name: "quantity",
+                message: "How many would you like to purchase?"
+            }
+        ]).then(function (answer) {
+
+            var quantity = parseInt(answer.quantity);
+            var product = parseInt(answer.product);
+
+            connection.query('SELECT * FROM products', function (err, product) {
+                if (err) throw err;
+
+                for (var i = 0; i < product.length; i++) {
+                    if (quantity <= product[i].stock_quantity && product === product[i].item_id) {
+
+                        connection.query(
+                            "UPDATE products SET ? WHERE ?",
+                            [
+                                {
+                                    stock_quantity: (product[i].stock_quantity - quantity)
+                                },
+                                {
+                                    id: product[i].item_id
+                                }
+                            ],
+                            function (err, update) {
+                                if (err) throw err;
+                                console.log("Thanks for purchasing " + quantity + " of " + product[i].product_name);
+                            }
+                        )
+
+                    } else if (quantity > data[i].stock_quantity && product === data[i].item_id) {
+                        inquirer.prompt([
+                            {
+                                type: "list",
+                                name: "notEnough",
+                                message: "I'm sorry - we don't have enough in stock!",
+                                choices: ["Purchase a different quantity", "Purchase a different product", "Stop shopping"]
+                            }
+                        ]).then(function (notEnough) {
+
+                            if (overQuantity.tooMany === "Purchase a different quantity" || overQuantity.tooMany === "Purchase a different product") {
+                                customerShop();
+                            
+                            } else {
+                                console.log("Thanks for visiting Bamazon!");
+                                connection.end();
+                            }
+                        });
+                    }
+                }
+            });
+        });
+    });
+}
